539df2cb4430b91ff492329ad264d5fc92b3a030
Jean Silva <jeancarlo199797@gmail.com>
2018-07-26 10:19:56 -0600

    Version 0.8 and gulp

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+/* Gulpfile.js */
+let gulp = require('gulp')
+let gutil =  require('gulp-util')
+let sass = require('gulp-sass')
+let webserver = require('gulp-webserver');
+let path = require('path')
+
+/* tasks */
+// gulp.task(
+//   name : String,
+//   deps : [] :: optional,
+//   cb : fn
+// )
+
+/* Styles task */
+gulp.task('styles', () => {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sass({includePaths: [
+                path.join(__dirname, 'node_modules/bootstrap-sass/assets/stylesheets'),
+                path.join(__dirname, 'src/scss')]
+            , outputStyle: 'compressed'}))
+        .pipe(gulp.dest('dist/css/'))
+})
+
+gulp.task('html', () => {
+    return gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('img', () => {
+    return gulp.src('src//**/*.png')
+        .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('watch', () => {
+    gulp.watch('src/scss/**/*.scss', ['styles'],cb => cb)
+    gulp.watch('src/**/*.html', ['html'],cb => cb)
+    gulp.watch('src/img/**/*.png', ['img'],cb => cb)
+})
+
+gulp.task('server', () => {
+    gulp.src('dist/')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }))
+})
+
+gulp.task('start', [
+    'html',
+    'styles',
+    'server',
+    'watch'
+], cb => cb)
